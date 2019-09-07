a961545e5be358a1c1de82ea53833713074bce67
Matheus Kindrazki <matheus@mentores.com.br>
2019-09-07 17:41:06 -0300

    Updates

---
+++ precache-manifest.de6d82cb1c8983736de8b8f6b4ab833e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f44684effd11825c1d686f3e29774ba5",
+    "url": "/Kindshoes/index.html"
+  },
+  {
+    "revision": "7e64b6c3dfcd85cbd08e",
+    "url": "/Kindshoes/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "7e64b6c3dfcd85cbd08e",
+    "url": "/Kindshoes/static/js/2.caf24fe8.chunk.js"
+  },
+  {
+    "revision": "01a28a2d612072da5205",
+    "url": "/Kindshoes/static/js/main.9ed4ea9b.chunk.js"
+  },
+  {
+    "revision": "c284bed9aa3d4162c6fc",
+    "url": "/Kindshoes/static/js/runtime~main.8383b14f.js"
+  },
+  {
+    "revision": "3c9a1ce505a48f2c15ba9505b6f5667b",
+    "url": "/Kindshoes/static/media/background.3c9a1ce5.svg"
+  },
+  {
+    "revision": "13e445a6fbb677b32725460867655f23",
+    "url": "/Kindshoes/static/media/logo.13e445a6.svg"
+  }
+]);
